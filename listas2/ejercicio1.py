5f589e668a33f861618b167b0e3ab092f50ab22f
Daniel <daniel.rsi6oa@gmail.com>
2025-10-16 17:53:17 -0300

    ejercicio1.py

--- listas2/ejercicio1.py
+++ listas2/ejercicio1.py
@@ -11,14 +11,14 @@ import random
 
 def menu_opciones():
     print('1. Generar N ternas de valores en forma aleatoria y agregarlas a una lista.')
-    print('Mostrar la lista de valores generados')
+    print('2. Mostrar la lista de valores generados')
     print('3. Calcular la suma de todos los valores generados')
     print('4. Calcular la suma de los valores de una terna a elección del usuario')
     print('5. Calcular el promedio de una columna (primera, segunda o tercera) de las ternas a elección del usuario')
-    print('6 Salir.')
+    print('6. Salir.')
     eleccion=int(input('1, 2, 3, 4, 5 o 6: '))
     while eleccion not in (1, 2, 3, 4, 5, 6):
-        eleccion(int(input('Debe ser 1, 2, 3, 4, 5 o 6: ')))
+        eleccion=(int(input('Debe ser 1, 2, 3, 4, 5 o 6: ')))
     return eleccion
 
 def generar_ternas():
@@ -30,9 +30,8 @@ def generar_ternas():
             numero_random = random.randint(0, 100)
             if validar_repeticion_lista(terna, numero_random):
                 terna.append(numero_random)
-                print('terna',terna)
         lista_ternas.append(terna)
-    print(lista_ternas)
+    return lista_ternas
 
 def validar_repeticion_lista(lista, numero):
     validar=True
@@ -40,20 +39,55 @@ def validar_repeticion_lista(lista, numero):
         validar=False
     return validar
 
+def mostrar_lista(lista):
+    print(lista)
 
+def sumar_valores_lista(lista):
+    acumulador=0
+    for listas in lista:
+        for num in listas:
+            acumulador+=num       
+    print(acumulador)
 
+def sumar_valores_terna(lista):
+    acumulador=0
+    copia_lista=lista.copy()
+    for i in range(len(lista)):
+        print(f'i:{i}, tupla:{lista[i]}')
+    indice_terna=int(input('Elegir indice de tupla a sumar: '))
+    terna_elegida=copia_lista.pop(indice_terna)
+    for num in terna_elegida:
+        acumulador+=num
+    print(acumulador)
 
-
-
-
-
+def promedio_columnas_lista(lista):  
+    acumulador=0
+    for listas in lista:
+        print(listas)
+    columna_elegida=int(input('Elegir columna 1, 2, 3, para calcular el promedio: '))
+    while columna_elegida not in (1, 2, 3):
+        columna_elegida=int(input('Solo tiene 3 columnas: '))
+    for listas in lista:
+        acumulador+=listas[columna_elegida-1]
+    promedio=acumulador/len(lista)
+    print(promedio)
 
 
 #PRINCIPAL
+lista_ternas=[]
 while True:
     opcion=menu_opciones()
     if opcion==1:
-        generar_ternas()
+        lista_ternas=generar_ternas()
+    elif opcion==2:
+        mostrar_lista(lista_ternas)
+    elif opcion==3:
+        sumar_valores_lista(lista_ternas)
+    elif opcion==4:
+        sumar_valores_terna(lista_ternas)
+    elif opcion==5:
+        promedio_columnas_lista(lista_ternas)
     elif opcion==6:
         break
+    input('Presione enter para continuar...')
 
