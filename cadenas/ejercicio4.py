9219bc8e466caf92979dac575d9e857239b18ffc
Daniel <daniel.rsi6oa@gmail.com>
2025-10-23 11:25:19 -0300

    ejercicio4

--- cadenas/ejercicio4.py
+++ cadenas/ejercicio4.py
@@ -3,11 +3,32 @@
  un mensaje de error."""
 
 clave=str(input('ingrese palabra clave: '))
-tiene_numero=False
+validar_caracteres=True
+if len(clave)<6:
+    validar_caracteres=False
+    print('tiene +6 caracteres?', validar_caracteres)
+else:
+    print('Tiene +6 caracteres?', validar_caracteres)
+
+validar_numero=False
 for i in range(len(clave)):
     if clave[i].isdigit():
-        tiene_numero=True
+        validar_numero=True
+        print('tiene numero?', validar_numero)
+        break
+if not validar_numero:
+    print('Tiene numero?', validar_numero)
 
-if len(clave)<6:
-    print('Debe tener mas de 6 caracteres')
-elif no
+validar_mayus=False
+for i in range(len(clave)):
+    if clave[i].isupper():
+        validar_mayus=True
+        print('tiene mayuscula?', validar_mayus)
+        break
+if not validar_mayus:
+    print('tiene mayuscula?', validar_mayus)
+
+if not validar_caracteres or not validar_numero or not validar_mayus:
+    print('La clave debe tener mas de 6 caracteres, un numero y una letra en mayuscula')
+else:
+    print('Clave valida')
